527c41f8cd3db3161eed3cbe5136c711386bbc30
intelligent.farming.platform@gmail.com <intelligent.farming.platform@gmail.com>
2020-10-27 22:48:51 +0700

    remove cookie

--- index.js
+++ index.js
@@ -9,14 +9,7 @@ var helmet = require("helmet");
 var session = require("express-session");
 
 app.set("trust proxy", 1); // trust first proxy
-app.use(
-  session({
-    secret: "s3Cur3",
-    name: "sessionId",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+
 app.use(helmet());
 //Import Routes
 const authRoute = require("./routes/auth");
--- routes/auth.js
+++ routes/auth.js
@@ -55,7 +55,7 @@ router.post("/login", async (req, res) => {
 
   //Create and assign a token
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
-    expiresIn: '5m', //1week
+    expiresIn: '5m', 
   });
   // res.json({
   //   'auth-token':token
